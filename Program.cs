29be14a61b1f0f73c53a4008b0580b146710e5ab
Matt-Matthew <mateusz.jaworski04@gmail.com>
2026-03-11 18:48:30 +0100

    Dodalem do StatisticsHelper metode do wypisywania elementow tablicy

--- Program.cs
+++ Program.cs
@@ -3,5 +3,11 @@ Console.WriteLine("Hello, World!");
 
 public class StatisticsHelper
 {
-    
+    public static void printNumbers(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
 }
