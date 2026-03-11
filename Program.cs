91bc9017a3a48cf98bff3e9133c3e5d9045c9d79
Matt-Matthew <mateusz.jaworski04@gmail.com>
2026-03-11 18:51:15 +0100

    Dodalem dane i przetestowalem metode do wypisywania elementow tablicy

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+StatisticsHelper.printNumbers(numbers);
 public class StatisticsHelper
 {
     public static void printNumbers(int[] numbers)
